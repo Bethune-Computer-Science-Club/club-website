ba6e7a4094f007dc902dac4a5b8579aa276c3134
QqWwEe123123123 <xxsimonxx119@gmail.com>
2021-03-06 17:31:55 -0500

    created navbar files

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Nav, NavbarContainer } from './Navbar.elements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          test
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
