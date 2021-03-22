fcdda413975c323d3fa94bc096a85d49ceed7f48
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-03-21 22:29:17 -0400

    semi-demoable state

--- src/pages/RoboticsPage/Data.js
+++ src/pages/RoboticsPage/Data.js
@@ -1,1 +1,23 @@
-// Data (use later)
+import BCSCLogo from '../../images/BCSCLogo.png'
+
+/*
+key: 0,
+title: "Hello, World!",
+authors: "",
+description: "This is where you will put a short description of your project. Don’t write too much, and try to aim for three sentences. Just include the most important parts.",
+projectName: "React Website"
+*/
+export const projectOne = {
+    lightBg: false, //true if bkg is white, false if blue
+    imgStart: true, //true if images is in the first column. False if image is in the second column
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    description: "Created by: The Bethune CS Club \n\n This is where you will put a short description of your project. Don’t write too much, and try to aim for three sentences. Just include the most important parts.",
+    headline: 'Bethune CS Club Website',
+    lightText: true, 
+    topLine: 'React Website',
+    img: BCSCLogo,
+    alt: 'Image',
+    showIframe: false,
+    showSchedule: false
+}
--- src/pages/RoboticsPage/Robotics.js
+++ src/pages/RoboticsPage/Robotics.js
@@ -1,38 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { InfoSection } from '../../components'
+import { projectOne } from './Data'
 
 function Robotics() {
 
-    // "blocks" is a section with information for a project
-    const [blocks, setBlocks] = useState([
-    {
-        key: 0,
-        title: "Hello, World!",
-        authors: "The Bethune CS Club",
-        description: "This is where you will put a short description of your project. Don’t write too much, and try to aim for three sentences. Just include the most important parts.",
-        projectName: "React Website"
-    },
-    {
-        key: 1,
-        title: "title",
-        authors: "idk",
-        description: "Lorem doesnt work here",
-        projectName: "tutorial simulator"
-    }
-    ])
-
     // functions for modifying state inside components
-    
-    
-    
-
-    return blocks.map(info => (
-        <>
-            <h1> Title: {info.title} </h1>
-            <h2> projectName: {info.projectName} </h2>
-            <h2> authors: {info.authors} </h2>
-            <h3> description: {info.description}</h3>
-        </>
-    ))
+    return (
+        <InfoSection {...projectOne}></InfoSection>
+    )
 }
 
 export default Robotics
